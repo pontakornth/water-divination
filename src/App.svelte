28f1c259c6a65468347a06816d5d167c2b3c51c8
pontakornth <pontakorn61@gmail.com>
2020-05-01 18:17:09 +0700

    init: Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+import Divination from './components/Divination.svelte';
+</script>
+
+<main>
+	<Divination />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 500px;
+		margin: auto;
+	}
+	@media (min-width: 680px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Divination.svelte
@@ -0,0 +1,37 @@
+<script>
+import { onMount } from 'svelte';
+let canvas;
+let width = 800;
+let height = 800;
+
+onMount(() => {
+    const ctx = canvas.getContext('2d');
+
+    //Draw water
+    ctx.beginPath();
+    ctx.fillStyle = 'skyblue';
+    ctx.arc(width / 2,height / 2,200,0,Math.PI * 2);
+    ctx.fill();
+    
+
+    //Draw aura
+    ctx.beginPath();
+    ctx.strokeStyle = 'skyblue';
+    ctx.arc(width / 2,height / 2,250,0,Math.PI * 2);
+    ctx.stroke();
+
+    //Draw rect
+    ctx.beginPath();
+    ctx.fillStyle = 'lightgreen';
+    ctx.rect(width / 2 - 50, height / 2 - 50,100,100);
+    ctx.fill();
+    
+})
+
+</script>
+
+<canvas 
+  bind:this={canvas} 
+  width="800"
+  height="800"
+/>
